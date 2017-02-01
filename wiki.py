e75451e04e67001bde3adceb83219df004382892
Brad <bradtaniguchi@gmail.com>
2017-01-31 23:23:20 -0800

    Wrapped code into AdminFrame object
    The AdminFrame object extends the ttk.Frame (use ttk when possible, it
    looks nicer) I also provided basic run code for this Frame. (its at the
    bottom of the file, with root and what not) I also formatted the
    AdminFrame object to standards so we can easily see what is going on.
    Following the format as followed:
    
    1. Create the widget (such as button, label, etc)
    2. Apply formatting/listeners (I only applied formatting )
    3. Apply to grid (I use no arguments for the grid, so it functions like
    pack(), but in the future grid() will be needed for more complex layous.
    
    Finally I added ttk.Style(), and applied the "best" looking style for
    linux. Idk what it will look like elsewhere, but it looks better than
    normal so we will use the theme.

--- wiki.py
+++ wiki.py
@@ -1,25 +1,51 @@
 #!/usr/bin/env python3
 
-#Could be used for administrator
+# Vigeneareay Phriang
+# Bradley Taniguchi
+# 1/31/17
+# Could be used for administrator
 
-from tkinter import *
+import tkinter as tk
 from tkinter import ttk
-root = Tk()
-root.title("Administrator Log In")
 
-Label(root, text="Username").pack()
-entry = ttk.Entry(root, width = 30)
-entry.pack()
-entry.get() #input
+class AdminFrame(ttk.Frame):
+    def __init__(self, master=None):
+        tk.Frame.__init__(self, master)  # make the frame
+        self.master = master  # the master is the tkinter window manager
+        self.grid()  # use the grid layout manager
+        self.create_widgets()  # call the create widget function
 
-Label(root, text="Password").pack()
-entry2 = ttk.Entry(root, width = 30)
-entry2.pack()
+    def create_widgets(self):
+        self.master.title("Administrator Log In")
+        self.username_label = ttk.Label(self, text="Username")
+        self.username_entry = ttk.Entry(self, width=30)
+        self.password_label = ttk.Label(self, text="Password")
+        self.password_entry = ttk.Entry(self, width=30)
+        self.loginbutton = ttk.Button(self, text = "Submit", width = 10)
+        
+        #apply special formatting
+        self.password_entry.config(show = '*')
 
-loginbutton = ttk.Button(root, text = "Submit", width = 10)
-loginbutton.pack()
-loginbutton['text']
-entry2.config(show = '*')
-entry.get()
 
-root.mainloop()
+        # now we add the components to the grid
+        # we wont use pack(), as grid is better for more complex layouts
+        # but as of right now it will function the same as pack()
+        self.username_label.grid()
+        self.username_entry.grid()
+
+        self.password_label.grid()
+        self.password_entry.grid()
+        
+        self.loginbutton.grid()
+
+# These are for quick testing of this file, 
+root = tk.Tk() #  root is the window
+
+# lets use a theme!
+root.style = ttk.Style()
+#('clam', 'alt', 'default', 'classic')
+root.style.theme_use("clam")
+
+root.minsize(width=400, height=400)
+app = AdminFrame(master=root)
+app.mainloop()
