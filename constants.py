7a1773070973ce8ab8aaf71445b8b9c6d70a574f
Brad <bradtaniguchi@gmail.com>
2017-01-31 16:30:02 -0800

    Added the main runner, and constants files
    The main file runs the tkinter application program (which doesn't exist
    yet) The constants file will provide some basic options, such as colors
    and other database constants (which also doesn't exist yet)
    
    For now the program just prints 'Hello world'

---
+++ constants.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# 
+class Colors(object):
+    """
+    Color constants for IMSTimesheet
+    """
+    MAIN_COLOR="#f9f9f9"
+    RED="#d9534f"
+    LIGHT_BLUE="#bc0de"
+    GREEN="#5cb85c"
+    BLUE="#428bca"
+
+class DatabaseConstants():
+    TEST="this is a test"
---
+++ main.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+from constants import Constants
+
+def main():
+    print("Hello World")
+
+
+if __name__ == '__main__':
+    main()
