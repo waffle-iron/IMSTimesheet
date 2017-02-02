4726b6458b52d2eb8bf71ae5a6d229d437c48a74
Rudy Leiva <rudyleiva93@yahoo.com>
2017-02-01 17:57:05 -0800

    Check to see if IMSTimesheet.db file exists

--- databaseInterface.py
+++ databaseInterface.py
@@ -1,6 +1,24 @@
 #!/usr/bin/python3
 
 import sqlite3
+import os
+class databaseInterface:
+    'Handles all database functions'
+    dbFile = "IMSTimesheet.db"
+    conn = sqlite3.connect(dbFile)
 
-#conn = sqlite3.connect('')
+    if os.path.isfile(dbFile):
+        print("yes!");
+
+
+
+"""
+Put db in a folder
+Check if database exists
+print ("opened database succesfully");"""
+
+
+
+
+
 
