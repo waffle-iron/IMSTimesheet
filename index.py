bd1d862adafd09e9c1118d4f31b79263c902cff5
vphriang1 <vphriang21@gmail.com>
2017-02-02 18:11:31 -0800

    Added the combobox dropdown menu.

--- index.py
+++ index.py
@@ -2,7 +2,6 @@ from tkinter import *
 import tkinter as tk
 from tkinter import ttk
 
-
 class EmployeeHome(ttk.Frame):
     def __init__(self, master=None):
         tk.Frame.__init__(self, master)
@@ -12,39 +11,37 @@ class EmployeeHome(ttk.Frame):
 
     def create_widgets(self):
         self.master.title("Employee Clock In System")
+        month = StringVar()
+        #set value of the combobox
+        self.month2 = ttk.Combobox(self, textvariable=month)
+        self.month2.config(values=('Jan', 'Feb', "March"))
+        self.combobox = ttk.Combobox(self, textvariable=month)
+        self.combobox.config(values=('Jan', 'Feb', "March"))
 
-        #match the resizing
-        self.rowconfigure(0, weight = 3)
-        self.rowconfigure(1, weight = 3)
-        self.columnconfigure(0, weight = 3)
-        self.columnconfigure(1, weight = 3)
+        print(month.get())
+        month.set('Jan')
+        month.set('Not a month!')
 
+        #match the resizing of the window
+        self.rowconfigure(0, weight=3)
+        self.rowconfigure(1, weight=3)
+        self.columnconfigure(0, weight=3)
+        self.columnconfigure(1, weight=3)
+
+        #instantiate all buttons
         self.clock_in =ttk.Button(self, text = 'Clock In')
         self.clock_out =ttk.Button(self, text = 'Clock Out')
         self.admin=ttk.Button(self, text = 'Administrator')
         self.printRecords=ttk.Button(self, text = 'Print Records')
 
-        self.clock_in.grid(row=0, column=0, padx = 10, pady=10,ipadx = 25, ipady=25)
-        self.clock_out.grid(row=1, column =0, padx = 10, pady=10, ipadx = 25, ipady=25)
-        self.admin.grid(row=0, column=1, padx = 10, pady=10, ipadx = 25, ipady=25)
-        self.printRecords.grid(row=1, column=1, padx = 10, pady=10, ipadx = 25, ipady=25)
-
-        mb = Menubutton(self, text="condiments", relief=RAISED)
-        mb.grid()
-        mb.menu = Menu(mb, tearoff=0)
-        mb["menu"] = mb.menu
-
-        mayoVar = IntVar()
-        ketchVar = IntVar()
-
-        mb.menu.add_checkbutton(label="mayo",
-                                variable=mayoVar)
-        mb.menu.add_checkbutton(label="ketchup",
-                                variable=ketchVar)
-
+        #render them to the tkinter gui
+        self.clock_in.grid(row=0, column=0, padx = 10, pady=10,ipadx = 10, ipady=10)
+        self.clock_out.grid(row=1, column =0, padx = 10, pady=10, ipadx = 10, ipady=10)
+        self.printRecords.grid(row=2, column=0, padx = 10, pady=10, ipadx = 10, ipady=10)
+        self.combobox.grid(row=2, column=3, padx = 10, pady=10, ipadx = 10, ipady=10)
+        self.combobox.grid(row=1, column=3, padx=10, pady=10, ipadx=10, ipady=10)
+        self.month2.grid(row=0, column=3, padx=10, pady=10, ipadx=10, ipady=10)
 
 root = tk.Tk()
-
-
 app1= EmployeeHome(master=root)
 app1.mainloop()
