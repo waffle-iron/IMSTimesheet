05bbde593914996a22af5fb6d5a9675fccf0b572
Rudy Leiva <rudyleiva93@yahoo.com>
2017-02-14 19:57:05 -0800

    Modified db file and databaseInterface

--- databaseInterface.py
+++ databaseInterface.py
@@ -7,15 +7,21 @@ import os
 class databaseInterface:
     '''Handles all database functions'''
     dbFile = "IMSTimesheet.db"
-    # conn = sqlite3.connect(dbFile)
+    conn = sqlite3.connect(dbFile)
 
-    # if not os.path.isfile(dbFile):
-        # print("no!")
-        # dbFile = open("IMSTimesheet.db", "w")
-        # conn = sqlite3.connect(dbFile)
-        # print("Opened datatbase")
+    if not os.path.isfile(dbFile):
+         print("no!")
+         dbFile = open("IMSTimesheet.db", "w")
+         conn = sqlite3.connect(dbFile)
+         print("Opened datatbase")
 
-        # conn.execute("")
+         conn.execute('''CREATE TABLE TIME_SHEET
+                      ( AutoIncrememnt INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+                      Name TEXT NOT NULL,
+                      DateIn TEXT NOT NULL,
+                      DateOut TEXT NOT NULL,
+                      ValidEntry INTEGER NOT NULL );''')
+
 
     #else:
         #print("Yes!")
