75fd3dd05409bf30e8bd8c3a547289be9a822aaa
Brad <bradtaniguchi@gmail.com>
2017-02-21 14:46:54 -0800

    Changed database interface to OOP,new rebuild.py
    I changed the database interface to follow traditional OOP design
    practicies. So now it is like a java class. I also added a few utility
    functions to the class to assist in integration.
    
    The rebuild.py file is a quick script that calls the new database
    functions to delete and hopefully rebuild the database file.

---
+++ rebuild.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+# This file deletes the database and rebuilds it using the database
+# interface. This is to assist in development
+
+from app.database.databaseInterface import databaseInterface
+
+print("Deleting database")
+di = databaseInterface()  # create the database object
+# NOTE the above line creates the database
+di.delete_database()  # delete database
+di.check()  # check if exist, if it doesn't create it
+
