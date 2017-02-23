c37dd6edff92a6cfb2574fe2238465e6defb3628
Brad <bradtaniguchi@gmail.com>
2017-02-23 12:27:48 -0800

    Moved rebuild back, fixed database creation
    Moving rebuild messed things up, because when the program executes it
    starts from the main.py, so the database WILL be in the local root
    folder without any customization.
    So it only makes sense to leave the rebuild.py in the root folder as
    well.
    
    I also updated the databaseinterface to save the connection to the
    database, this added a small issue of creating the database agian, but
    this was also fixed.

--- app/database/databaseInterface.py
+++ app/database/databaseInterface.py
@@ -10,10 +10,20 @@ class DatabaseInterface:
     Authors: 
         Rudy Leiva
         Bradley Taniguchi
+    Todo: Add exception handling to all sql functions, and save the connections
+        to prevent redundant opening and closing of connections. 
     """
     def __init__(self, dbFile="IMSTimesheet.db"):
-        self.dbFile = dbFile 
+        self.dbFile = dbFile
         self.check()  # execute the check of the database
+        self.conn = sqlite3.connect(dbFile) # object database conn
+       
+    def __del__(self):
+        """
+        deconstructor method, used to close the database connection.
+        """
+        self.conn.close()
+
 
     def check(self):
         """
@@ -23,10 +33,6 @@ class DatabaseInterface:
         if not os.path.isfile(self.dbFile):
             print("No Databse found")
             self.create_database()  # if it doesn't exist create it!
-            #print("no!")
-            #dbFile = open("IMSTimesheet.db", "w")
-            #conn = sqlite3.connect(dbFile)
-            #print("Opened database")
 
     
     def create_database(self):
@@ -34,15 +40,14 @@ class DatabaseInterface:
         Creates the database file using the database filename provided to the
         database object upon init.
         """
-        print("creating database...")
-        conn = sqlite3.connect(self.dbFile)
+        print("creating database")
+        conn = sqlite3.connect(self.dbFile) # note this is a LOCAL conneciton, just to create the database
         conn.execute('''CREATE TABLE TIME_SHEET
-                     ( AutoIncrememnt INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-                     Name TEXT NOT NULL,
-                     DateIn TEXT NOT NULL,
-                     DateOut TEXT,
-                     ValidEntry INTEGER NULL );''')
-        conn.close()
+                         ( AutoIncrememnt INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+                         Name TEXT NOT NULL,
+                         DateIn TEXT NOT NULL,
+                         DateOut TEXT,
+                         ValidEntry INTEGER NULL );''')
 
     def punch_in(self, Name, DateIn):
         """
@@ -54,12 +59,18 @@ class DatabaseInterface:
             DateIn (str): string representation of the date, be sure the
                 format is determined beforehand, as the clockout does not logic
                 to check the date format.
+        Returns: boolean value if the punch in was successful
         """
         self.check()
-        conn = sqlite3.connect(self.dbFile)
-        conn.execute("INSERT INTO TIME_SHEET (Name,DateIn) \
-                     VALUES (?, ?)", (Name.lower(), DateIn));
-        conn.close()
+        #lets check to see if the user is already clocked in
+        if( not self.check_user(Name)):
+            self.conn.execute("INSERT INTO TIME_SHEET (Name,DateIn) \
+                        VALUES (?, ?)", (Name.lower(), DateIn));
+            return true
+        else: 
+            #user is already clocked in!
+            return false
+
     def check_user(self, Name):
         """
         Checks if the Name is already clocked in according to the system.
@@ -70,35 +81,36 @@ class DatabaseInterface:
                 name, where their clock out times is null is GREATER than 1.
         """
         self.check()
-        conn = sqlite3.connect(self.dbFile)
-        users = conn.execute("SELECT * FROM TIME_SHEET \
+        users = self.conn.execute("SELECT * FROM TIME_SHEET \
                 WHERE Name=? AND \
                 DateOut is NULL", Name.lower()).fetchall()
-        conn.close()
         return (len(users) >= 1)
 
-    def punch_out(self, Name):
+    def punch_out(self, Name, DateOut, ValidEntry=1):
         """
         clocks-out a user with the given name, this function clocks out the
         LAST login time. The database should not be filled with more than 1
         Args:
             Name (str): Name of the user to clock out from the database
                 we apply to lower case just incase
+            DateOut (str): Date and time of the clockout to put.
+            ValidEntry (int): If the entry is valid
+                defaults to 1 for valid
         Todo: Fix this function, as it doesn't do whats intended
         """
         self.check()
-        conn = sqlite3.connect(self.dbFile)
-#        conn.execute("INSERT INTO TIME_SHEET (Name,DateIn) \
-#                    VALUES (?, ?)", (Name.lower(), DateOut));
-        conn.close()
+        self.conn.execute("UPDATE TIME_SHEET SET \
+                    DateOut=?, ValidEntry=1 WHERE\
+                    Name=? AND DateOut is NULL", DateOut, Name)
+        
 
-    def delete_database(self):
-        """
-        utility function to delete the database file.
-        """
-        print("deleting database")
-        os.remove(self.dbFile)
-        print("deleted Database!")
+def delete_database():
+    """
+    utility function to delete the database file.
+    """
+    print("deleting database")
+    os.remove("IMSTimesheet.db")
+    print("deleted Database!")
 
 
 """
---
+++ rebuild.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+# This file deletes the database and rebuilds it using the database
+# interface. This is to assist in development
+
+from app.database.databaseInterface import delete_database
+from app.database.databaseInterface import DatabaseInterface
+
+delete_database()  # delete database
+di = DatabaseInterface()
+di.check()  # check if exist, if it doesn't create it
+
